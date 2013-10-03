93d6144314193bb60def99ba11559240574ab3d0
zhengjia <jiazheng@live.com>
2013-10-02 20:33:46 -0500

    update install

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,9 @@ dir=~/dotfiles
 files="bashrc bash_profile vimrc vim gitignore ackrc gitconfig"
 
 for file in $files; do
-  ln -s $dir/$file ~/.$file
+  if [[ ! -L ~/.$file ]]; then
+    ln -s $dir/$file ~/.$file
+  fi
 done
 
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
